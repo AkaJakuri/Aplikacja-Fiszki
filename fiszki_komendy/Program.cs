be484e3e83d778ca0638411672865d4252df5f38
jakuri <akajakuri@gmail.com>
2025-01-09 18:40:29 +0100

    dodanie losowania słowa i sprawdzenia tłumaczenia.

--- fiszki_komendy/Program.cs
+++ fiszki_komendy/Program.cs
@@ -3,4 +3,20 @@ using fiszki_komendy;
 
 baza_danych baza = new baza_danych();
 
-baza.hello();
+var wynik = baza.Słowo();
+string słowo = wynik.Item1;
+string id = wynik.Item2;
+Console.WriteLine(słowo);
+
+
+
+//Console.WriteLine("Podaj swoje imię:");
+//string próba = Console.ReadLine() ?? "";
+
+
+
+string próba = "ezz";
+
+bool czy;
+czy =baza.sprawdzenie(słowo, id, próba);
+Console.WriteLine(czy);
--- fiszki_komendy/baza_danych.cs
+++ fiszki_komendy/baza_danych.cs
@@ -15,8 +15,10 @@ namespace fiszki_komendy
         string connectionString = @"Server=DESKTOP-JMS3Q0H\SQLEXPRESS;Database=Fiszki;Trusted_Connection=True ;Encrypt=False";
 
 
-        public void hello() {
+        public (string, string) Słowo() {
             Console.WriteLine("hello");
+            string ang_slowo = "xxx";
+            string id_string = "sss";
             try
             {
                 // Tworzymy obiekt połączenia
@@ -25,7 +27,7 @@ namespace fiszki_komendy
                     connection.Open();
                     Console.WriteLine("Połączenie z bazą danych nawiązane!");
 
-                    string query = "select * from fiszki";
+                    string query = "SELECT TOP 1 id, slowo FROM Fiszki ORDER BY NEWID();";
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
                         using (SqlDataReader reader = command.ExecuteReader())
@@ -33,18 +35,54 @@ namespace fiszki_komendy
                         {
                             while (reader.Read()) 
                             {
+
                                 int id = reader.GetInt32(0);
+                                id_string = id.ToString();
                                 string slowo = reader.GetString(1);
                                 Console.WriteLine($"id= {id}, słowo= {slowo}");
+                                ang_slowo = slowo.ToLower();
                             }
-
-
                         }
-
-
                     }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Błąd połączenia: {ex.Message}");
+            }
+            return (ang_slowo,id_string);
 
-                    
+        }
+
+
+        public bool sprawdzenie(string ang_slowo, string id, string proba)
+        {
+            Console.WriteLine($"{ang_slowo},{id},{proba}");
+            bool wynik = false;
+            string tlumaczenie ="";
+            try
+            {
+                // Tworzymy obiekt połączenia
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Połączenie z bazą danych nawiązane!");
+
+                    string query = $"select tlumaczenie from fiszki where id={id}";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+
+                        {
+                            while (reader.Read())
+                            {
+                                tlumaczenie = reader.GetString(0);
+                                Console.WriteLine(tlumaczenie);
+
+                                
+                            }
+                        }
+                    }
                 }
             }
             catch (Exception ex)
@@ -52,9 +90,26 @@ namespace fiszki_komendy
                 Console.WriteLine($"Błąd połączenia: {ex.Message}");
             }
 
+            if (tlumaczenie == proba) 
+            {
+                wynik = true;
+                Console.WriteLine("sie udało ");
 
+            }
+            else
+            {
+                Console.WriteLine("nie da się");
+            }
+
+
+            return wynik;
+            
 
         }
 
+
     }
+
+
 }
+
